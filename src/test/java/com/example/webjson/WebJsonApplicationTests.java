922bfbf6275edecd87137c33e1f4e5599be81548
93294 <9329496@mail.ru>
2022-12-05 16:51:17 +0300

    Initial commit

---
+++ src/test/java/com/example/webjson/WebJsonApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.webjson;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebJsonApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
